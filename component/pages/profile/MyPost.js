cabf95302fb6f7eec5d0894d7e4f68f4a705b902
nuru14 <ncahyo737@gmail.com>
2019-03-19 18:55:50 +0700

    set hexgrid in my post or profil

--- component/pages/profile/MyPost.js
+++ component/pages/profile/MyPost.js
@@ -6,19 +6,18 @@ import {
   Divider,
   Icon,
   GridColumn,
-  GridRow,
   List,
-  Image,
-  Popup,
-  Modal,
-  Button,
   Header,
+  Image,
+  Modal,
   Label,
-  Comment,
-  Form
+  Button,
+  Popup
 } from "semantic-ui-react";
+
 import Skeleton from "react-skeleton-loader";
 import axios from "axios";
+import "./Hex.css"
 
 export default class MyPost extends Component {
   constructor(props) {
@@ -187,6 +186,7 @@ export default class MyPost extends Component {
   render() {
     const { posting, isLoading } = this.state;
     const nopost = posting.length;
+    // const hexagons = GridGenerator.parallelogram(-1, 2, -1, 0);
     const gridMargin = {
       marginBottom: "40px"
     };
@@ -216,16 +216,25 @@ export default class MyPost extends Component {
         ) : (
           <Container>
             <Segment basic>
+            
+            <ul id="grid" className="clear" style={{marginTop: -45}}>
               {posting.map((data, index) => {
                 return (
-                  <Grid columns={1} key={data._id}>
-                    <GridColumn>
-                      <GridRow>
-                        <List style={gridMargin}>
-                          <List.Item>
-                            <List.Content>
-                              <List.Header as="a">
-                                <small>
+            
+                    <li key={data._id}>
+                      <div className="hexagon">       
+                        { data.fotocontent !== null ? 
+                          (<Modal 
+                            onClose={this.close} closeIcon
+                            trigger={<Image
+                              src={"http://localhost:3000/src/web-api/public/posting/foto/" + data.fotocontent}
+                              style={{
+                                height: "100%",
+                                width: "100%"
+                            }}
+                            />} 
+                            >
+                            <Header ><small>
                                   {data.tags === "null" ? (
                                     <Image
                                       src="http://192.168.1.14/assets/icons/tags/pilihkategori.png"
@@ -284,79 +293,97 @@ export default class MyPost extends Component {
                                 </small>
                                 <small>
                                   <i style={textMargin}>{data.tags}</i>
-                                </small>
-                                <Modal
-                                  trigger={<Label onClick={this.handleOpen} style={{color: "black", border: "1", background: "white", float: "right", marginRight: "-4%"}}><Icon name="trash alternate outline"/></Label>}
-                                  open={this.state.modal}
-                                  onClose={this.handleClose}
-                                  basic
-                                >
-                                  <Header icon="trash alternate outline" content="Delete Posting!" />
-                                  <Modal.Content>
-                                    <p>Are You Sure?</p>
-                                  </Modal.Content>
-                                  <Modal.Actions>
-                                  <Button onClick={this.handleClose} inverted>
-                                    <Icon name="remove" /> No
-                                  </Button>
-                                  <Button inverted onClick={() => this.delete(data._id)}>
-                                    <Icon name="checkmark" /> Yes
-                                  </Button>
-                                  </Modal.Actions>
-                                </Modal>
-                              </List.Header>
-                              <br />
-                              <List.Description>
-                              { data.fotocontent !== null ? 
-                                (<Modal trigger={<Image
+                                </small></Header>
+
+                              <Modal.Content>
+                                <Image
                                   src={"http://localhost:3000/src/web-api/public/posting/foto/" + data.fotocontent}
-                                  size="large" />} 
-                                >
-                                <Header/>
-                                  <Modal.Content>
-                                    <Image
-                                      src={"http://localhost:3000/src/web-api/public/posting/foto/" + data.fotocontent}
-                                      size="big" />
-                                      <a onClick= {() => this.discuss(data.id_posts)}>{this.state.comment}comment</a>
-                                  </Modal.Content>
-                                </Modal>) : null
-                              }
-                                <br />
-                                <br />
-                                <b>{data.content}</b>
-                                <br />
-                                <br />
-                                  <Popup trigger={
-                                  <Icon
-                                    name="handshake outline"
-                                    onClick={() => this.givethanks(data._id)}
-                                  />}>{this.state.kode == 1 ? "Anda Telah Thanks" 
-                                      : "Anda Telah UnThanks"}
-                                  </Popup>
-                                <small>
-                                  <i>{data.thanks} Thanks </i>
-                                </small>
-                                <br/>
-                                <br/>
-                                <a onClick= {() => this.discuss(data.id_posts)}>comment</a>
-                                <small style={{ float: "right" }}>
-                                <i>
-                                {
-                                  data.date.slice(11) == this.state.year ? data.date.slice(4, -5) == this.state.datemonth ? data.jam == this.state.jam ? data.menit == this.state.menit ? "Now" 
-                                  : this.state.menit - data.menit + " m ago"  : this.state.jam - data.jam + " h ago" : data.date.slice(4, -5) : data.date.slice(4)
-                                }
-                                </i>
-                                </small>
-                              </List.Description>
-                            </List.Content>
-                          </List.Item>
-                        </List>
-                        <Divider fitted />
-                      </GridRow>
-                    </GridColumn>
-                  </Grid>
+                                  />
+                                <Grid>
+                                  <Grid.Row columns={4}>
+                                    <Grid.Column>
+                                      {/* thanks */}
+                                      <Popup trigger={
+                                      <Icon
+                                        name="handshake outline"
+                                        size='large'
+                                        onClick={() => this.givethanks(data._id)}
+                                      />}>{this.state.kode == 1 ? "Anda Telah Thanks" 
+                                          : "Anda Telah UnThanks"}
+                                      </Popup>
+                                      <br />
+                                      <small>
+                                        <i> {data.thanks} Thanks </i>
+                                      </small>
+                                    </Grid.Column>
+                                    <Grid.Column style={{marginLeft: -50}}>
+                                      {/* Coment */}                                
+                                      <Icon 
+                                        name="facebook messenger"
+                                        size='large'
+                                        onClick= {() => this.discuss(data.id_posts)}
+                                      />{this.state.comment}
+                                    </Grid.Column>
+                                    <Grid.Column></Grid.Column>
+                                    
+                                    <Grid.Column style={{marginLeft: 50}}>
+                                      {/* delete */}
+                                      <Modal
+                                        trigger={
+                                          
+                                          <Icon onClick={this.handleOpen} name="trash alternate outline" size='large'style={{marginLeft: 30}}/>
+                                        }
+                                        open={this.state.modal}
+                                        onClose={this.handleClose}
+                                        basic
+                                      >
+                                        <Header icon="trash alternate outline" content="Delete Posting!" />
+                                        <Modal.Content>
+                                          <p>Are You Sure?</p>
+                                        </Modal.Content>
+                                        <Modal.Actions>
+                                        <Button onClick={this.handleClose} inverted>
+                                          <Icon name="remove" /> No
+                                        </Button>
+                                        <Button inverted onClick={() => this.delete(data._id)}>
+                                          <Icon name="checkmark" /> Yes
+                                        </Button>
+
+                                        </Modal.Actions>
+                                      </Modal>
+                                    </Grid.Column>
+                                  </Grid.Row>
+                                  <Grid.Row columns={1}>
+                                    <GridColumn>
+                                      <b style={{fontSize: 15}}>{data.content}.</b>
+                                        <small>
+                                          <i>
+                                          {
+                                            data.date.slice(11) == this.state.year ? data.date.slice(4, -5) == this.state.datemonth ? data.jam == this.state.jam ? data.menit == this.state.menit ? "Now" 
+                                            : this.state.menit - data.menit + " m ago"  : this.state.jam - data.jam + " h ago" : data.date.slice(4, -5) : data.date.slice(4)
+                                          }
+                                          </i>
+                                        </small>
+                                        <br />
+                                        <br />
+                                 
+                                    </GridColumn>
+                                  </Grid.Row>
+                                  
+                                </Grid>                      
+                              </Modal.Content>
+                          </Modal>) : null
+                          }
+                                
+                      </div>
+                    </li>
                 );
               })}
+              </ul>
+              <Divider hidden/>
+              <Divider hidden/>
+              <Divider hidden/>
+              <Divider hidden/>
               <Divider hidden/>
             </Segment>
           </Container>
