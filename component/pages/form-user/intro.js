08f2c7ac069645a8ba6c6342b24588d832f87bf5
nuru14 <ncahyo737@gmail.com>
2019-03-27 16:56:19 +0700

    add intro and button login and register

--- component/pages/form-user/intro.js
+++ component/pages/form-user/intro.js
@@ -40,28 +40,36 @@ export default class intro extends Component {
         }=this.state;
         return(
             <div style={{backgroundImage: `url(${background})`, width: "100%", height: "100%", position: 'absolute', top: 0, left: 0, backgroundSize: 'cover'}}>
-              
-                <div columns='three' style={{
-                  
-                  background: "#5190ed",
-                
-                }}>
-                  
-                    
-                  <Icon name='sign-in alternate' size='large' onClick={() => window.location="#/login"} style={{float: "right", zIndex: 1,
+                <div style={{
+                  float: "right", 
+                  zIndex: 1,
                   position: "fixed",
                   bottom: 65,
                   color: "white",
-                  marginLeft: "110px"}}>Login</Icon>  
-                  <Icon name='share square outline' size='large' onClick={() => window.location="#/register"} style={{float: "left", zIndex: 1,
+                  marginLeft: "80px",
+                  borderRadius: "10px",
+                  padding: 4,
+                  border: "3px solid white"
+                }}>
+                  <Image centered src="./src/client/assets/images/icon/1.png" onClick={() => window.location="#/login"} style={{width: "50%", height: "50%"}}/>
+                  <center><span>Login</span></center>
+                </div>
+
+                <div style={{
+                  float: "left", 
+                  zIndex: 1,
                   position: "fixed",
                   bottom: 65,
                   right: 1,
                   color: "white",
-                  marginRight: "110px"}}>Register</Icon>
-                    
-                  
-              </div>
+                  marginRight: "80px",
+                  borderRadius: "10px",
+                  padding: 4,
+                  border: "3px solid white",
+                }}>
+                  <Image centered src='./src/client/assets/images/icon/2.png' onClick={() => window.location="#/register"} style={{width: "59%", height: "59%"}}/>
+                  <center><span>Register</span></center>
+                </div>
                                 
             </div>
         );
