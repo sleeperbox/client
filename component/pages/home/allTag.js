84309845b36bde9757353cc4dce4dbeeb0fe7233
teday <tedi.suryadi77@gmail.com>
2019-04-30 12:38:42 +0700

    Navigation Home

--- component/pages/home/allTag.js
+++ component/pages/home/allTag.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { TabComponent } from '@syncfusion/ej2-react-navigations';
-import { Divider, Image } from "semantic-ui-react";
+import { Image, Container } from "semantic-ui-react";
 import comp from '../../../assets/icon/komp.png'
 import fam from '../../../assets/icon/family.png'
 import fnr from '../../../assets/icon/f&r.png'
@@ -10,6 +10,15 @@ import quo from '../../../assets/icon/quotes.png'
 import rid from '../../../assets/icon/riddle.png'
 import oth from '../../../assets/icon/other.png'
 import fol from '../../../assets/icon/follow.png'
+import Content from "./content";
+import PostOther from "./PostingOther";
+import PostQuotes from "./PostingQuotes";
+import PostRiddles from "./PostingRiddles";
+import PostComputerGadget from "./PostingComputerGadget";
+import PostFamilyLove from "./PostingFamilyLove";
+import PostFactRumour from "./PostingFactRumour";
+import PostBussinessWork from "./PostingBussinessWork";
+import PostFashionLifestyle from "./PostingFashionLifestyle";
 
 
 const iconSizing = {
@@ -29,8 +38,8 @@ export default class Alltag extends Component {
   render() {
     return (
       <div style={{ position: "relative",top: 20, zIndex: 2}}>
-        <TabComponent id='defaultTab' overflowMode='Scrollable' style={{position: "fixed", background: "#fff", boxShadow: '0 8px 6px -6px #999999' }}>
-          <div className="e-tab-header">
+        <TabComponent id='defaultTab' overflowMode='Scrollable' style={{ background: "#fff", boxShadow: '0 8px 6px -6px #999999'}}>
+          <div className="e-tab-header" style={{position: "fixed", zIndex: 1}}>
             <div> <Image src={fol} style={iconSizing}/> You Follow </div>
             <div> <Image src={comp} style={iconSizing}/>&nbsp;Computer &amp; Gadget </div>
             <div> <Image src={fam}  style={iconSizing}/>&nbsp;Family &amp; Love </div>
@@ -41,41 +50,54 @@ export default class Alltag extends Component {
             <div> <Image src={rid} style={iconSizing}/> Riddles </div>
             <div> <Image src={oth}  style={iconSizing}/>&nbsp;Other </div>
           </div>
+          <br />
+          <br />
+          <br />
           <div className="e-content">
             <div>
-              {/* you follow */}
+              <Container>
+                <Content />
+              </Container>
             </div>
             <div>
-              {/* Computer &amp; Gadget */}
-              <p>opening computer and gadget</p>
+              <Container>
+                <PostComputerGadget />
+              </Container>
             </div>
             <div>
-              {/* Family &amp; Love */}
-              <p>opening family and love</p>
+              <Container>
+                <PostFamilyLove />
+                </Container>
             </div>
             <div>
-              {/* Fact &amp; Rumor */}
-              <p>opening fact and rumor</p>
+              <Container>
+                <PostFactRumour />
+              </Container>
             </div>
             <div>
-              {/* Business &amp; Work */}
-              <p>opening business and work</p>
+              <Container>
+                <PostBussinessWork />
+              </Container>
             </div>
             <div>
-              {/* Fashion &amp; Lifestyle */}
-              <p>opening fashion and lifestyle</p>
+              <Container>
+                <PostFashionLifestyle />
+              </Container>
             </div>
             <div>
-              {/* Quotes */}
-              <p>opening quotes</p>
+              <Container>
+                <PostQuotes />
+              </Container>
             </div>
             <div>
-              {/* Riddles */}
-              <p>opening riddles</p>
+              <Container>
+                <PostRiddles />
+              </Container>
             </div>
             <div>
-              {/*  Other */}
-              <p>opening other</p>
+              <Container>
+                <PostOther />
+              </Container>
             </div>
           </div>
         </TabComponent>
