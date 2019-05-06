4c1743f8a07dcb35c756f6440600ebc66732df92
deaaprizal <afrizalmiphone@gmail.com>
2019-05-06 09:58:49 +0700

    delete module camera

--- component/pages/home/Navbar.js
+++ component/pages/home/Navbar.js
@@ -4,72 +4,18 @@ import {
   Image,
   Modal
 } from "semantic-ui-react";
-import Camera from 'react-camera';
 import giftIco from "../../../assets/icon/gift.png";
 import cameraIco from "../../../assets/icon/camera.png";
 
 const egg = "https://media.giphy.com/media/YS65hkIsbZrMeK3VYf/giphy.gif"
-const style = {
-  preview: {
-    position: 'relative',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  },
-  captureImage: {
-    width: '100%',
-  }
-};
+
 export default class Navbar extends Component {
   constructor(props) {
     super(props)
     this.state = {
       open: false,
       openNotif: false
-    },
-    this.takePicture = this.takePicture.bind(this);
-  }
-
-  takePicture() {
-    this.camera.capture()
-    .then(blob => {
-      this.img.src = URL.createObjectURL(blob);
-      this.img.onload = () => { URL.revokeObjectURL(this.src); }
-    })
-  }
-
-  snapWay() {
-    return (
-      <div>
-        <Camera
-          style={style.preview}
-          ref="camera"
-        >
-          <div style={style.captureContainer} onClick={this.takePicture}>
-            <div style={style.captureButton} />
-          </div>
-        </Camera>
-        <img
-          style={style.captureImage}
-          ref={(img) => {
-            this.img = img;
-          }}
-        />
-      </div>
-    )
+    }
   }
 
   openModal() {
@@ -111,7 +57,7 @@ export default class Navbar extends Component {
         {this.state.openNotif ? this.notif() : null}
         {this.state.open ? this.openModal() : null}
           <Menu borderless size="large" fixed="top" style={{ background: "#5190ed" }}>
-            <Menu.Item name='camera' size="large" style={{ color: "white" }} onClick={() => this.snapWay()}>
+            <Menu.Item name='camera' size="large" style={{ color: "white" }}>
               <Image src={cameraIco} style={{ width: 25, height: 25 }} /> &nbsp;&nbsp;
               <p style={{ marginTop: -0, color: "#ffffff", fontFamily: "Trebuchet MS, Helvetica, sans-serif", fontStyle: "italic" }}> &nbsp;Wayhome</p>
             </Menu.Item>
